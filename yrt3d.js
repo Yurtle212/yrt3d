1dbe1e8da59f78c5c80fd211209639d20b3adab9
Lachlan Butler <yurtle.butler@gmail.com>
2022-04-15 21:02:22 -0700

    Added comments

--- yrt3d.js
+++ yrt3d.js
@@ -14,6 +14,7 @@ let centerY = 250
 
 let rotationSpeed = 0.2;
 
+// Read the mesh data
 function loadMesh(path) {
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", path, false);
@@ -37,15 +38,20 @@ function parseMesh(fileContents) {
     let onEdge = 0;
     let currentV;
 
+    // Loop through each line of the file
     for (line of lines) {
         let sLine = line.replace('\r', '').split(" ");
         for (let token in sLine) {
+            // Loop through each token in each line
             if (sLine[token] != "") {
                 if (token == 0) {
+                    // Check if it's a vertex or an edge.
                     if (sLine[token] in mesh.vertices) {
+                        // Add edge to mesh
                         type = 1;
                         mesh.edges[onEdge] = sLine[token];
                     } else {
+                        // Add new vertex to mesh
                         type = 0;
                         currentV = sLine[token];
                         mesh.vertices[currentV] = {};
@@ -54,9 +60,11 @@ function parseMesh(fileContents) {
                 } else {
                     switch (type) {
                         case 0:
+                            // Add x, y, or z coordinate to vertex.
                             mesh.vertices[currentV][String.fromCharCode(parseInt(token) + 119)] = parseFloat(sLine[token]);
                             break;
                         case 1:
+                            // Add second vertex to edge.
                             mesh.edges[onEdge] += " " + sLine[token];
                             onEdge++;
                             break;
@@ -70,41 +78,52 @@ function parseMesh(fileContents) {
 }
 
 function displayMesh() {
+    // Make the mesh rotate every 0.0333 seconds.
     let intervalID = window.setInterval(displayRotating, 0.0333);
 }
 
 function displayRotating() {
-    // Your code here
+    // Apply rotation to mesh.
     for (let vert in mesh.vertices) {
+        // Loop through each vertex and apply the rotation.
         rVerts[vert]["x"] = (mesh.vertices[vert]["x"] * Math.cos(radians(rotation))) - (mesh.vertices[vert]["z"] * Math.sin(radians(rotation)));
         rVerts[vert]["y"] = mesh.vertices[vert]["y"]
         rVerts[vert]["z"] = (mesh.vertices[vert]["x"] * Math.sin(radians(rotation))) + (mesh.vertices[vert]["z"] * Math.cos(radians(rotation)));
         console.log(rVerts[vert]);
     }
+    // Set the rotation for next time.
     rotation += rotationSpeed;
     if (rotation >= 360) {
         rotation = 0;
     }
+
+    // Draw the mesh
     drawMesh();
 }
 
 function drawMesh() {
+    // Draw the mesh
     for (let vert in rVerts) {
+        // Project 3d coordinates into 2d
         rVerts[vert]["x"] = centerX + ((rVerts[vert]["x"] / (rVerts[vert]["z"] + zOffset)) * size)
         rVerts[vert]["y"] = centerY + ((rVerts[vert]["y"] / (rVerts[vert]["z"] + zOffset)) * size)
     }
 
+    // Loop through each edge in the mesh
     for (let edge in mesh.edges) {
         let edgeVerts = mesh.edges[edge].split(" ");
 
+        // Try to get the edge in the HTML
         let xmlEdge = document.getElementById("e" + edge);
         if (xmlEdge == null) {
+            // If edge hasn't been drawn yet, make a new line.
             let parent = document.getElementById("yrt3dSvg");
             xmlEdge = document.createElementNS('http://www.w3.org/2000/svg','line');
             xmlEdge.setAttribute("style", "stroke:rgb(255,255,255);stroke-width:1");
             xmlEdge.setAttribute("id", "e" + edge)
             parent.appendChild(xmlEdge);
         }
+        // Set the coordinates for the line.
         xmlEdge.setAttribute("x1", rVerts[edgeVerts[0]]["x"]);
         xmlEdge.setAttribute("y1", rVerts[edgeVerts[0]]["y"]);
         xmlEdge.setAttribute("x2", rVerts[edgeVerts[1]]["x"]);
@@ -114,5 +133,6 @@ function drawMesh() {
 
 function radians(degrees)
 {
-  return degrees * (Math.PI/180);
+    // Convert degrees to radians
+    return degrees * (Math.PI/180);
 }
